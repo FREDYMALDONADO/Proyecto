8d683eae92f59c83fd393b5dd514172d285741dc
FREDYMALDONADO <mrfredymaldonado@gmail.com>
2022-02-18 08:24:32 -0700

    Nuevo archivo

--- aritmeticos_form.php
+++ aritmeticos_form.php
@@ -9,47 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Operadores Aritmeticos</title>
-
-   
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">PROYECTO</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Clases de PW
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Operadores aritmeticos</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-
-
+    <?php require_once"menu.php" ?>
 
   </head>
   <body>
@@ -57,6 +17,7 @@
 <div class="container"><br>
 <div class="row justify-content-center">
     <div class="col-6 p-5 bg-white shadow-lg rounded">
+    <h3>Operadores aritmeticos</h3>
 <form method="post" action="aritmetico.php">
     <div class="form-group">
         <label for="valor1">Valor1</label>
@@ -76,7 +37,7 @@
                 <option value="m">Multiplicar</option>
                 <option value="d">Dividir</option>
                 </select>
-            </div>
+            </div><br>
         
    
     <div class="form-group">
--- operadores_form.php
+++ operadores_form.php
@@ -9,46 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Operadores Aritmeticos</title>
-
-   
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">PROYECTO</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Clases de PW
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Operadores aritmeticos</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-
+    <?php require_once"menu.php" ?>
 
 
   </head>
@@ -57,6 +18,7 @@
 <div class="container"><br>
 <div class="row justify-content-center">
     <div class="col-6 p-5 bg-white shadow-lg rounded">
+    <h3>Operadores de comparacion</h3>
 <form method="post" action="operacion.php">
     <div class="form-group">
         <label for="valor1">Valor1</label>
@@ -69,14 +31,12 @@
         <input id="valor2" class="form-control" type="text" name="valor2">
 
         
-        
+        </div><br>
    
     <div class="form-group">
   <button class="btn btn-secondary" type="submit">Enviar</button>
   </div>
 </form>
-
-    </div>
 </div>
 
 
